f2d63b2e472d1ced1f72872bb854dee56277c38c
Henrique Dias Campos <henrique.campos@mv.com.br>
2020-03-29 23:03:38 -0300

    ola mundo

---
+++ src/olaMundo.js
@@ -0,0 +1,10 @@
+/**
+ * console.log é a funçao de saida de dados do javascript
+ * equivalente ao comando escreva do visualg
+ * 
+ * 
+ */
+
+
+console.log('ola mundo');
+console.log('me livrei da maldiçao');
